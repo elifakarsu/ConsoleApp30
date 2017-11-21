a8c14a959f5c32458781a305a28dc5080b168de7
moonlaughs <iza8848@gmail.com>
2017-11-21 13:04:06 +0100

    Update Program.cs
    added

--- ConsoleApp30/Program.cs
+++ ConsoleApp30/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace ConsoleApp30
         {
             //comment from Iza
             //elifs comment
+            //comment right from git
         }
     }
 }
